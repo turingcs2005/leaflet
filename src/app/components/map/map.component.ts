b5168453b2e35181c033b0783c960040a8ef1904
Rui Wang <rui.wang.624@gmail.com>
2021-04-08 22:02:12 -0400

    type annotation

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -8,7 +8,7 @@ import * as L from 'leaflet';
 })
 export class MapComponent implements OnInit {
 
-  private map;
+  private map: L.Map;
   private centroid: L.LatLngExpression = [42.3601, -71.0589]; //
 
   private initMap(): void {
