62659b5ce6d1d48571f61b6b3b8164184212f246
Rui Wang <rui.wang.624@gmail.com>
2021-04-08 22:54:06 -0400

    used forEach

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -23,12 +23,14 @@ export class MapComponent implements OnInit {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
-    // create 5 random jitteries
-    const jittery = Array(5).fill(this.centroid).map( x => [x[0] + (Math.random() - .5)/10, x[1] + (Math.random() - .5)/10 ]);
-
-    for (const i of jittery) {
-         const marker = L.marker(i as L.LatLngExpression).addTo(this.map);
-    }
+    // create 5 random jitteries and add them to map
+    const jittery = Array(5).fill(this.centroid).map( 
+        x => [x[0] + (Math.random() - .5)/10, x[1] + (Math.random() - .5)/10 ]
+      ).map(
+        x => L.marker(x as L.LatLngExpression)
+      ).forEach(
+        x => x.addTo(this.map)
+      );
 
     tiles.addTo(this.map);
   
